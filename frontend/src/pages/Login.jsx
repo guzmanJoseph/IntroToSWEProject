060a6edddb85e806e4f12fca9faa83313d794559
Joseph Guzman <149011223+guzmanJoseph@users.noreply.github.com>
2025-12-03 16:29:24 -0500

    Fixed error

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../api";
 import { useAuth } from "../contexts/AuthContext";
-import { Link } from "reach-router-dom"
+import { Link } from "react-router-dom"
 
 export default function Login() {
   const [email, setEmail] = useState("");
